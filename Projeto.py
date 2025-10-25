f32df703e133af92fe5d12f05ba58173984eb34b
Luís Oliveira <luismco12@gmail.com>
2025-10-25 17:38:31 +0100

    Version 0.1 - Ready for Testing

--- Projeto.py
+++ Projeto.py
@@ -1,112 +1,132 @@
 import ipaddress
 reguaList = [128,64,32,16,8,4,2,1]
+tools = ["1. Conversão de IP (Decimal para Binário)", 
+         "2. Conversão de IP (Binário para Decimal)", 
+         "3. Cálculo da Máscara de Rede/CIDR", 
+         "4. Classicação de IPs (Privado vs Público)", 
+         "5. Régua de Bits"
+         ]
 
-def header():
+def mainHeader():
     print("=" * 50)
     print("Projeto Final Python\nPycket Tracer - Ferramenta de Apoio a Networking\nDiogo Fontes | Luís Oliveira")
+
+def resultHeader():
+    print()
+    print("=" * 50)
+    print("Resultado")
+
+def resultFooter():
+    print("=" * 50 ,"\n")
+    submenu()
+
+def toolHeader():
+    print()
+    print("=" * 50)
+    print("Pycket Tracer Tools")
+    print(tools[int(option)-1])
     print("=" * 50)
 
-def ferramenta(opcao):
-    if opcao == 1:
-        print("=" * 50)
-        print("Pycket Tracer Tools")
-        print("Conversão de IP (Decimal para Binário)")
-        print("=" * 50)
+def tool(option):
+    if option == 1:
+        toolHeader()
         while True:
             try:
                 decimalIP = input("Insira um endereço de IPv4 em formato decimal: ")
                 ip = ('{:b}'.format(ipaddress.IPv4Address(decimalIP)))
-                print(f"{ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
-                break
+                resultHeader()
+                print(f"IP em formato binário: {ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
+                resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido")
-    elif opcao == 2:
-        print("=" * 50)
-        print("Pycket Tracer Tools")
-        print("Conversão de IP (Binário para Decimal)")
-        print("=" * 50)
+                print("Insira um IPv4 válido (Ex.: 192.168.1.1)")
+    elif option == 2:
+        toolHeader()
         while True:
             try:
-                decimalIP = input("Insira um endereço de IPv4 em formato decimal: ")
-                ip = ('{:b}'.format(ipaddress.IPv4Address(decimalIP)))
-                print(f"{ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
-                break
+                binaryIP = input("Insira um endereço de IPv4 em formato binário: ")
+                binaryIP = binaryIP.replace(".", "")
+                binaryIP = int(binaryIP, 2)
+                ip = ipaddress.IPv4Address(binaryIP)
+                resultHeader()
+                print(f"IP em formato decimal: {ip}")
+                resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido")
-    elif opcao == 3:
-        print("=" * 50)
-        print("Pycket Tracer Tools")
-        print("Conversão de IP (Binário para Decimal)")
-        print("=" * 50)
+                print("Insira um IPv4 válido (Ex.: 11000000.10101000.00000001.00000001)")
+    elif option == 3:
+        toolHeader()
         while True:
             try:
-                decimalIP = input("Insira um endereço de IPv4 em formato decimal: ")
-                ip = ('{:b}'.format(ipaddress.IPv4Address(decimalIP)))
-                print(f"{ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
-                break
+                hosts = int(input("Insira o número de dispositivos necessários: "))
+                totalHosts = hosts + 2
+                bits = 0
+                while (2 ** bits) < totalHosts:
+                    bits += 1
+                cidr = 32 - bits
+                network = ipaddress.ip_network(f"192.168.0.0/{cidr}")
+                resultHeader()
+                print(f"Máscara de Rede Adequeada: {network.netmask}")
+                print(f"CIDR adequado: /{cidr}")
+                print(f"Número de IPs disponíveis: {network.num_addresses - 2}")
+                resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido")
-    elif opcao == 4:
-        print("=" * 50)
-        print("Pycket Tracer Tools")
-        print("Conversão de IP (Binário para Decimal)")
-        print("=" * 50)
+                print("Insira um número inteiro")
+    elif option == 4:
+        toolHeader()
         while True:
             try:
-                decimalIP = input("Insira um endereço de IPv4 em formato decimal: ")
-                ip = ('{:b}'.format(ipaddress.IPv4Address(decimalIP)))
-                print(f"{ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
-                break
-            except ValueError:
-                print("Insira um IPv4 válido")
-    elif opcao == 5:
-        print("=" * 50)
-        print("Pycket Tracer Tools")
-        print("Conversão de IP (Binário para Decimal)")
-        print("=" * 50)
-        print(reguaList)
-        while True:
-            try:
-                opcao5 = input("Clicar 0 para voltar ao menu inicial ou enter para sair: ")
-                if opcao5 == "":
-                    exit()
-                elif int(opcao5) == 0:
-                    menu()
+                ip = input("Insira um endereço de IPv4: ")
+                if ipaddress.IPv4Address(ip).is_private is True:
+                    resultHeader()
+                    print(f"O IP '{ip}' é um IP Privado")
+                    resultFooter()
                 else:
-                    print("Insira apenas 0 ou clique enter para sair")
+                    resultHeader()
+                    print(f"O IP '{ip}' é um IP Público")
+                    resultFooter()
             except ValueError:
-                print("Insira apenas 0 ou clique enter para sair")
+                print("Insira um IPv4 válido (Ex.: 192.168.1.1)")
+    elif option == 5:
+        toolHeader()
+        print(reguaList)
+        print("=" * 50 ,"\n")
+        submenu()
 
 def menu():
+        global option
+        print("=" * 50)
         print("Pycket Tracer Tools")
         print("=" * 50)
-        print("1. Conversão de IP (Decimal para Binário)")
-        print("2. Conversão de IP (Binário para Decimal)")
-        print("3. Cálculo da Máscara de Rede/CIDR")
-        print("4. Classicação de IPs (Privado vs Público)")
-        print("5. Imprimir a régua de bits")
-        print("0. Sair")
+        print(*tools,sep="\n")
         while True:
             try:
-                opcao = int(input("Selecione a ferramenta desejada: "))
-                if opcao >= 1 and opcao < 6:
-                    break
-                elif opcao == 0:
+                option = input("Selecione a ferramenta desejada (Clique enter para sair): ")
+                if option == "":
                     exit()
+                elif int(option) >= 1 and int(option) <= 5:
+                    tool(int(option))
                 else:
-                    print("Insira apenas opções entre 0 e 5")
+                    print("Insira apenas opções entre 1 e 5")
             except ValueError:
-                 print("Insira apenas opções entre 0 e 5")
-        if opcao == 1:
-            ferramenta(opcao)
-        elif opcao == 2:
-            ferramenta(opcao)
-        elif opcao == 3:
-            ferramenta(opcao)
-        elif opcao == 4:
-            ferramenta(opcao)
-        elif opcao == 5:
-            ferramenta(opcao)
+                print("Insira apenas opções entre 1 e 5")
 
-header()
+def submenu():
+    print("1. Voltar ao menu inicial")
+    print("0. Sair")
+    while True:
+        try:
+            subOption = input("Selecione a opção desejada (Clique enter para continuar na ferramenta atual): ")
+            if subOption == "":
+                print()
+                tool(int(option))
+            elif int(subOption) == 1:
+                print()
+                menu()
+            elif int(subOption) == 0:
+                exit()
+            else:
+                print("Insira apenas opções entre 0 e 1")
+        except ValueError:
+            print("Insira apenas opções entre 0 e 1")
+
+mainHeader()
 menu()
