cdbfe298a45f30270021eecfe3603fcd09a404c4
Luís Oliveira <luismco12@gmail.com>
2025-10-25 20:01:29 +0100

    Version 0.2 - Adds new functions

--- Projeto.py
+++ Projeto.py
@@ -1,11 +1,26 @@
 import ipaddress
-reguaList = [128,64,32,16,8,4,2,1]
+import random
+
+regua = [128,64,32,16,8,4,2,1]
 tools = ["1. Conversão de IP (Decimal para Binário)", 
          "2. Conversão de IP (Binário para Decimal)", 
          "3. Cálculo da Máscara de Rede/CIDR", 
          "4. Classicação de IPs (Privado vs Público)", 
          "5. Régua de Bits"
          ]
+
+def defaultDecimalIP():
+    randIP = []
+    for x in range(4):
+        randIP.append(str(random.randrange(0,256)))
+    return ".".join(randIP)
+
+def defaultBinaryIP():
+    randIP = []
+    for x in range(32):
+        randIP.append(str(random.randrange(0,2)))
+    randIP = "".join(randIP)
+    return f"{randIP[0:9]}.{randIP[9:17]}.{randIP[17:25]}.{randIP[25:33]}"
 
 def mainHeader():
     print("=" * 50)
@@ -17,14 +32,15 @@ def resultHeader():
     print("Resultado")
 
 def resultFooter():
-    print("=" * 50 ,"\n")
+    print("=" * 50, "\n")
     submenu()
 
 def toolHeader():
     print()
     print("=" * 50)
     print("Pycket Tracer Tools")
-    print(tools[int(option)-1])
+    toolTitle = tools[int(option)-1]
+    print(toolTitle[3:])
     print("=" * 50)
 
 def tool(option):
@@ -38,7 +54,7 @@ def tool(option):
                 print(f"IP em formato binário: {ip[0:9]}.{ip[9:17]}.{ip[17:25]}.{ip[25:33]}")
                 resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido (Ex.: 192.168.1.1)")
+                print(f"Insira um IPv4 válido (Ex.: '{defaultDecimalIP()}')")
     elif option == 2:
         toolHeader()
         while True:
@@ -51,22 +67,28 @@ def tool(option):
                 print(f"IP em formato decimal: {ip}")
                 resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido (Ex.: 11000000.10101000.00000001.00000001)")
+                print(f"Insira um IPv4 válido (Ex.: '{defaultBinaryIP()}')")
     elif option == 3:
         toolHeader()
         while True:
             try:
                 hosts = int(input("Insira o número de dispositivos necessários: "))
+                decimalIP = input("Insira um endereço de IPv4 dentro da rede: ")
+                ip = ('{:b}'.format(ipaddress.IPv4Address(decimalIP)))
                 totalHosts = hosts + 2
                 bits = 0
                 while (2 ** bits) < totalHosts:
                     bits += 1
                 cidr = 32 - bits
-                network = ipaddress.ip_network(f"192.168.0.0/{cidr}")
+                network = ipaddress.ip_network(f"{ip}/{cidr}")
                 resultHeader()
-                print(f"Máscara de Rede Adequeada: {network.netmask}")
-                print(f"CIDR adequado: /{cidr}")
-                print(f"Número de IPs disponíveis: {network.num_addresses - 2}")
+                print(f"""Máscara de Rede Adequeada: {network.netmask}
+                CIDR adequado: /{cidr}
+                Número de IPs disponíveis: {network.num_addresses - 2}
+                IP da rede: {network.network_address}
+                Primero IP Disponível: {network.hosts[1]}
+                Último IP Disponível: {network.hosts[-1]}
+                IP de Broadcast: {network.broadcast_address}""")
                 resultFooter()
             except ValueError:
                 print("Insira um número inteiro")
@@ -84,12 +106,11 @@ def tool(option):
                     print(f"O IP '{ip}' é um IP Público")
                     resultFooter()
             except ValueError:
-                print("Insira um IPv4 válido (Ex.: 192.168.1.1)")
+                print(f"Insira um IPv4 válido (Ex.: '{defaultDecimalIP()}')")
     elif option == 5:
         toolHeader()
-        print(reguaList)
-        print("=" * 50 ,"\n")
-        submenu()
+        print(regua)
+        resultFooter()
 
 def menu():
         global option
--- teste.py
+++ teste.py
@@ -1,11 +1,19 @@
 import ipaddress
 
-
-while True:
-    try:
-        ip = input("ip")
-        print(ip)
-        ipcheck = ipaddress.IPv4Address(ip).is_private
-        print(ipcheck)
-    except ValueError:
-        print("Try again")
+hosts = int(input("Insira o número de dispositivos necessários: "))
+decimalIP = input("Insira um endereço de IPv4 dentro da rede: ")
+ip = ipaddress.IPv4Address(decimalIP)
+totalHosts = hosts + 2
+bits = 0
+while (2 ** bits) < totalHosts:
+    bits += 1
+cidr = 32 - bits
+network = ipaddress.ip_network(f"{ip}/{cidr}")
+print(f"""
+    Máscara de Rede Adequeada: {network.netmask}
+    CIDR adequado: /{cidr}
+    Número de IPs disponíveis: {network.num_addresses - 2}
+    IP da rede: {network.network_address}
+    Primero IP Disponível: {ipaddress.IPv4Network(network)[1]}
+    Último IP Disponível: {ipaddress.IPv4Network(network)[-2]}
+    IP de Broadcast: {network.broadcast_address}""")
