aec8ca706cfd6ae1967a942209a0578237e2f944
bluisholive <luismco12@gmail.com>
2025-10-29 16:24:56 +0000

    Adds VLSM

--- teste.py
+++ teste.py
@@ -1,28 +1,78 @@
-import hashlib
+import ipaddress
 import random
-import getpass
-import hashlib
 from textwrap import dedent
+import hashlib
+import getpass
 import string
+ 
+def vlsm():
+    print("\033c", end="")
+    print(dedent(f"""
+        {"=" * 50}
+        VLSM
+        {"=" * 50}"""))
+    while True:
+        try:
+            n_networks = int(input("Number of Networks to Configure: "))
+            if n_networks < 1:
+                print("Only positive integers allowed")
+            else:
+                break
+        except ValueError:
+            print("Only positive integers allowed")
+    networks = {}
+    for x in range(n_networks):
+        while True:
+            try:
+                network_input = int(input(f"Enter the number of hosts for Network {x+1}: "))
+                if network_input < 1:
+                    print("Only positive integers allowed")
+                else:
+                    networks[f'network_{x}'] = {
+                        'needed_hosts': network_input,
+                        'needed_ips': network_input + 2,
+                        'network_mask': None,
+                        'cidr': None,
+                        'hosts': None,
+                        'network_ip': None,
+                        'first_ip': None,
+                        'last_ip': None,
+                        'broadcast_ip': None
+                    }
+                    break
+            except ValueError:
+               print("Only positive integers allowed")
+    counter = 0
+    while counter < n_networks:
+        cidr = 0
+        bits = 0
+        totalHosts = networks[f'network_{counter}']['needed_ips']
+        while (2 ** bits) < totalHosts:
+            bits += 1
+            cidr = (32 - bits)
+        networks[f'network_{counter}']['cidr'] = cidr
+        counter += 1
+    sorted_networks = dict(sorted(networks.items(), reverse=True, key=lambda item: item[1]['needed_hosts']))
+    while True:
+        try:
+            network0_ip = input("Insert the initial Network IP: ")
+            ip = ipaddress.IPv4Address(network0_ip)
+            network0 = ipaddress.ip_network(f"{ip}/{sorted_networks[list(sorted_networks)[0]]['cidr']}")
+            break
+        except ValueError:
+            print("Try again")
+    networks[list(sorted_networks)[0]]['network_mask'] = network0.netmask
+    networks[list(sorted_networks)[0]]['hosts'] = (network0.num_addresses - 2)
+    networks[list(sorted_networks)[0]]['network_ip'] = network0.network_address
+    networks[list(sorted_networks)[0]]['first_ip'] = network0[1]
+    networks[list(sorted_networks)[0]]['last_ip'] = network0[-2]
+    networks[list(sorted_networks)[0]]['broadcast_ip'] = network0.broadcast_address
+    print(networks)
 
-database = {
-    'luis': {'password': '20f15cfb78a1c83af3bd7976a78952ea1b1ed435a706bb04ba2c83c7fd0a4965', 'role': 'admin'}, 
-    'diogo': {'password': '9ca6a0e5e922e01e20f11d999ecc1685e969c9acc2abc83006281c131fe22a15', 'role': 'admin'},
-    'ruben': {'password': 'c1cc69e61c0f1c7ade8df0f2994e582e7c1f2c57d1ec192a0baf9f96b7739d9d', 'role': 'user'}
-    }
 
-tools = ["1. Conversão de IP (Decimal para Binário)", 
-         "2. Conversão de IP (Binário para Decimal)", 
-         "3. Cálculo da Máscara de Rede/CIDR", 
-         "4. Classicação de IPs (Privado vs Público)",
-         "5. Alterar Password",
-         "6. Terminar sessão",
-         "7. Administração de Utilizadores"
-        ]
+    
 
-while True:
-    hosts = input("Insira o número de dispositivos necessários: ")
-    if hosts is not int:
-          print("Insira apenas números inteiros")
-    break
+
+
+vlsm()
 
